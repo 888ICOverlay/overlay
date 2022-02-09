2d2c7fe1b8de04f1738b5473a79d27b596b1696f
AyronEth <aaron76526@gmail.com>
2022-02-09 02:05:06 -0800

    updating w/ new ralms

--- public/js/index.js
+++ public/js/index.js
@@ -9,46 +9,60 @@ const ctx = canvas.getContext("2d");
 ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 // Images
-let selectedFile = null;
+let selectedFileName = null;
 const circle = new Image(1080, 1080);
 circle.crossOrigin = "anonymous";
 circle.src = "https://i.ibb.co/gT3NyL3/888-Inner-Circle-Maalavidaa-1.png";
+circle.onload = function (){
+    rebuildCanvas()
+}
+
+inputImg = new Image();
+inputImg.width = 1080;
+inputImg.height = 1080;
+inputImg.src = circle.src
 
 const handleNewFile = (file) => {
-  const reader = new FileReader();
+    const reader = new FileReader();
 
-  reader.readAsDataURL(file);
-  reader.onloadend = (event) => {
-    const image = new Image();
-    image.src = event.target.result;
-    image.onload = function () {
-      console.log("Building canvas...");
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
-      const width = (canvas.width = Math.min(image.width, circle.width));
-      const height = (canvas.height = Math.min(image.height, circle.height));
-      ctx.drawImage(image, 0, 0, width, height);
-      ctx.drawImage(circle, 0, 0, width, height);
-      ctx.globalCompositeOperation = "destination-in";
-      ctx.beginPath();
-      ctx.arc(width / 2, height / 2, height / 2, 0, Math.PI * 2);
-      ctx.closePath();
-      ctx.fill();
-      console.log("Finished building canvas");
-      selectedFile = file;
+    reader.readAsDataURL(file);
+    reader.onloadend = (event) => {
+        inputImg = new Image();
+        inputImg.src = event.target.result;
+        inputImg.onload = function(){
+            selectedFileName = file.name;
+            rebuildCanvas();
+        };
     };
-  };
+};
+
+rebuildCanvas = function () {
+    console.log("Building canvas...");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    const width = (canvas.width = Math.min(inputImg.width, circle.width));
+    const height = (canvas.height = Math.min(inputImg.height, circle.height));
+    if(selectedFileName) {
+        ctx.drawImage(inputImg, 0, 0, width, height);
+    }
+    ctx.drawImage(circle, 0, 0, width, height);
+    ctx.globalCompositeOperation = "destination-in";
+    ctx.beginPath();
+    ctx.arc(width / 2, height / 2, height / 2, 0, Math.PI * 2);
+    ctx.closePath();
+    ctx.fill();
+    console.log("Finished building canvas");
 };
 
 // Handle new image selection
 fileInput.onchange = function (e) {
-  console.log("Received", e.target.files[0], "from file input");
-  handleNewFile(e.target.files[0]);
+    console.log("Received", e.target.files[0], "from file input");
+    handleNewFile(e.target.files[0]);
 };
 
 // Handle drag-and-drop
 const preventDefault = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
+    e.preventDefault();
+    e.stopPropagation();
 };
 
 root.addEventListener("dragenter", preventDefault, false);
@@ -57,30 +71,37 @@ root.addEventListener("dragover", preventDefault, false);
 root.addEventListener("drop", preventDefault, false);
 
 root.addEventListener(
-  "drop",
-  (e) => {
-    const file = e.dataTransfer?.files[0];
+    "drop",
+    (e) => {
+        const file = e.dataTransfer?.files[0];
 
-    if (file) {
-      handleNewFile(file);
-    }
-  },
-  false
+        if (file) {
+            handleNewFile(file);
+        }
+    },
+    false
 );
 
 // Handle download
 downloadButton.onclick = function () {
-  console.log("Attemping to download canvas");
-  if (!selectedFile) return console.log("No selected image!");
+    console.log("Attempting to download canvas");
+    if (!selectedFileName) return console.log("No selected image!");
 
-  const linkElement = document.createElement("a");
+    const linkElement = document.createElement("a");
 
-  const fileName =
-    selectedFile.name.substring(0, selectedFile.name.lastIndexOf(".")) + ".png";
+    const fileName = selectedFileName.substring(0, selectedFileName.lastIndexOf(".")) + ".png";
 
-  console.log("Preparing", fileName, "for download");
-  linkElement.download = fileName;
-  linkElement.href = canvas.toDataURL("image/png");
+    console.log("Preparing", fileName, "for download");
+    linkElement.download = fileName;
+    linkElement.href = canvas.toDataURL("image/png");
 
-  linkElement.click();
+    linkElement.click();
 };
+
+$(".grid-item").on("click",
+    function () {
+        $(".grid-item").removeClass("grid-item-active");
+        $(this).toggleClass("grid-item-active");
+        circle.src = $(this).attr('src');
+        rebuildCanvas();
+    });
